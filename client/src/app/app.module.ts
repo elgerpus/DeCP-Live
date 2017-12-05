857863d2ae61a514fb2107a44becb88bd116a6e6
Hilmar Tryggvason <hilmart@gmail.com>
2017-12-05 09:39:44 +0000

    installed ng2-materialized dependency

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterializeModule } from "ng2-materialize";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MaterializeModule.forRoot(),
+        FormsModule,
+        RouterModule.forRoot([
+            // {
+            //     path: "",
+            //     pathMatch: "full",
+            //     component: AppComponent
+            // }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
